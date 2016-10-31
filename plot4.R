e6880dad327e6eb2e0c48a65a2251901c235013c
GaheeLee12345 <gaheelee12345@unist.ac.kr>
2016-10-31 19:02:39 +0900

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,27 +1,28 @@
-##plot4
-data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+Sys.setenv("LANGUAGE"="En")
+Sys.setlocale("LC_ALL", "English")
+# Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
 
-data <- data %>% mutate(V1 = as.POSIXct(dmy_hms(as.character(paste(V1, V2)))),
-                        V3 = as.numeric(as.character(V3)),
-                        V4 = as.numeric(as.character(V4)),
-                        V5 = as.numeric(as.character(V5)),
-                        V7 = as.numeric(as.character(V7)),
-                        V8 = as.numeric(as.character(V8)),
-                        V9 = as.numeric(as.character(V9)))
+# initiating a composite plot with many graphs
+par(mfrow=c(2,2))
 
-
-par(mfrow = c(2, 2))
-
-with(data, plot(V1,V3, type="l", xlab = "", ylab = "Global Active Power (kilowatts)"))
-
-with(data, plot(V1,V5, type="l", xlab = "datetime", ylab = "Voltage"))
-
-with(data, plot(V1,V7, type="n", xlab = "", ylab = "Energy Sub Metering"))
-with(data, points(V1,V7, col="black", type="l"))
-with(data, points(V1,V8, col="red", type="l"))
-with(data, points(V1,V9, col="blue", type="l"))
-legend("topright", lty=1, col = c("black", "red", "blue"), 
-       legend = c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3"))
-
-with(data, plot(V1,V4, type="l", xlab = "datetime", ylab = "Global_reactive_power"))
+# calling the basic plot function that calls different plot functions to build the 4 plots that form the graph
+with(subpower,{
+  plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",  xlab="",ylab="Global Active Power")  
+  plot(subpower$Time,as.numeric(as.character(subpower$Voltage)), type="l",xlab="datetime",ylab="Voltage")
+  plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+  with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+  with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+  with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+  legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.3,y.intersp = 0.5, bty="n")
+  plot(subpower$Time,as.numeric(as.character(subpower$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
+})
