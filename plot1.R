dc9a8516b296b8d8f1fbc5b5f28e8a1bb8b5d669
GaheeLee12345 <gaheelee12345@unist.ac.kr>
2016-10-31 18:59:22 +0900

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,12 @@
-#reading
+
+#Reading, naming and subsetting power consumption data
 setwd("C:/Users/gahee/Documents/2016_Unist/2016_2nd/R/week9")
-data=read.table("household_power_consumption.txt",sep=';',skip=66637, nrows = 2880)
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-data=data %>% select(V3) %>% mutate(V3=as.numeric(as.character(V3)))
+#calling the basic plot function
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
 
-#1
-hist(data$V3, col="red",main="Global Active Power", xlab="Global Active Power (Killowatts)", ylab="Frequency")
+# annotating graph
+title(main="Global Active Power")
