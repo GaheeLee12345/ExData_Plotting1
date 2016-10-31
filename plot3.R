fefcca00ffed0ea478f9cdd3b8314461d7686c31
GaheeLee12345 <gaheelee12345@unist.ac.kr>
2016-10-31 19:02:03 +0900

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,16 +1,24 @@
-##plot3
-data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+
+Sys.setenv("LANGUAGE"="En")
+Sys.setlocale("LC_ALL", "English")
+# Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
 
-data <- data %>% mutate(V1 = as.POSIXct(dmy_hms(as.character(paste(V1, V2)))),
-                        V7 = as.numeric(as.character(V7)),
-                        V8 = as.numeric(as.character(V8)),
-                        V9 = as.numeric(as.character(V9))) %>% select(V1,V7:V9)
+# calling the basic plot functions
+plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.6, pch=1, pt.cex = 0.3)
 
-##plot
-with(data, plot(V1,V7, type="n", xlab = "", ylab = "Energy Sub Metering"))
-with(data, points(V1,V7, col="black", type="l"))
-with(data, points(V1,V8, col="red", type="l"))
-with(data, points(V1,V9, col="blue", type="l"))
-legend("topright", lty=1, col = c("black", "red", "blue"), 
-       legend = c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3"))
+# annotating graph
+title(main="Energy sub-metering")
