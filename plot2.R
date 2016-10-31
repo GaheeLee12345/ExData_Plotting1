ed3832accfe2cae5d768cc57162be00842a2f5f3
GaheeLee12345 <gaheelee12345@unist.ac.kr>
2016-10-31 19:01:28 +0900

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,8 +1,19 @@
-#plot 2
-data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+Sys.setenv("LANGUAGE"="En")
+Sys.setlocale("LC_ALL", "English")
+# Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
 
-data <- data %>% mutate(V1 = as.POSIXct(dmy_hms(as.character(paste(V1, V2)))),
-                        V3 = as.numeric(as.character(V3))) %>% select(V1,V3)
+# calling the basic plot function
+plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
 
-with(data, plot(V1,V3, type="l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+# annotating graph
+title(main="Global Active Power Vs Time")
